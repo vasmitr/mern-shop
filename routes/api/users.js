12e6923c05245dde0413cf3e477c3a75c6021f69
vasmitr <vasmitr95@gmail.com>
2018-08-19 22:04:38 +0500

    Add user login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Models
 const User = require('../../models/User');
 
-//Validators
+// Validators
 const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
+
+// @route   POST /api/users/register
+// @desc    Register new user
+// @access  Public
 router.post('/register', (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
 
@@ -36,6 +42,43 @@ router.post('/register', (req, res) => {
                 });
             });
         }
+    });
+
+});
+
+// @route   POST /api/users/login
+// @desc    Return JWT-token
+// @access  Public
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const { email, password } = req.body;
+
+    User.findOne({email}).then((user) => {
+        if (!user) {
+            errors.email = 'User with this email doesn\'t exists';
+            return res.status(404).json(errors)
+        }
+
+        bcrypt.compare(password, user.password).then((isMatch) => {
+            if (!isMatch) {
+                errors.password = 'Incorrect password';
+                return res.status(400).json(errors);
+            } else {
+                const payload = {id: user.id, name: user.name};
+
+                jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: 3600}, (err, token) => {
+                    return res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    });
+                });
+            }
+        });
     });
 
 });
