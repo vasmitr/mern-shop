4021cbc2573a3e15f049a24f5acee70ed8a9a046
vasmitr <vasmitr95@gmail.com>
2018-08-19 19:23:38 +0500

    connected mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+// Load config
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const users = require('./routes/api/users');
 const products = require('./routes/api/products');
@@ -6,6 +10,12 @@ const orders = require('./routes/api/orders');
 
 
 const app = express();
+
+// DB settings
+const db = process.env.MONGO_URI;
+mongoose.connect(db)
+    .then(() => console.log('Mongo connecteg'))
+    .catch((err) => console.log(err));
 
 app.use('/api/users', users);
 app.use('/api/products', products);
