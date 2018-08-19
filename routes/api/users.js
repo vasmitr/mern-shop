0fc393efc3914466422d2339943bb90b70a69597
vasmitr <vasmitr95@gmail.com>
2018-08-19 20:56:43 +0500

    Encrypt passwords

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 // Models
 const User = require('../../models/User');
@@ -24,9 +25,16 @@ router.post('/register', (req, res) => {
                 email: req.body.email,
                 password: req.body.password
             });
-            newUser.save()
-                .then((user) => res.json(user))
-                .catch((err) => console.log(err));
+
+            // Hash user's password
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    newUser.password = hash;
+                    newUser.save()
+                        .then((user) => res.json(user))
+                        .catch((err) => console.log(err));
+                });
+            });
         }
     });
 
