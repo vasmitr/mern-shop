04af5a2155502c248a9760cde54a1731735f1a2b
vasmitr <vasmitr95@gmail.com>
2018-08-21 15:27:41 +0500

    Add updatind functionality

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -9,14 +9,15 @@ const validateCategoryInput = require('../../validations/category');
 const validateProductInput = require('../../validations/product');
 
 
-// @route   POST /api/products/category
-// @desc    Create category
+// @route   POST /api/admin/category/:id?
+// @desc    Create or update category
 // @access  Private
 router.post(
-    '/categories/create',
+    '/category/:id?',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
         let errors = {};
+        const categoryId = req.params.id;
         
         if (!req.user.admin) {
             errors.notadmin = 'Only admin can create things there'
@@ -28,28 +29,40 @@ router.post(
                 return res.status(400).json(errors);
             }
 
-            const newCategory = new Category({
+            const categoryFields = {
                 name: req.body.name,
                 description: req.body.description,
                 image: req.body.image
-            });
-    
-            Category.create(newCategory)
-            .then(category => res.json(category))
-            .catch(err => console.log(err));
+            };
+
+            if (categoryId) {
+                // Update
+                Category.findByIdAndUpdate(categoryId, categoryFields)
+                .then(category => res.json({category}))
+                .catch(() => {
+                    errors.category = 'Category with this ID is no longer exists';
+                    return res.status(404).json(errors)
+                })
+            } else {
+                // Create
+                Category.create(categoryFields)
+                .then(category => res.json(category))
+                .catch(err => console.log(err));
+            }            
         }
     }
 );
 
 
-// @route   POST /api/products/create
-// @desc    Create or product
+// @route   POST /api/admin/product/:id?
+// @desc    Create or update product
 // @access  Private
 router.post(
-    '/products/create',
+    '/product/:id?',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
         let errors = {}
+        const productId = req.params.product_id;
 
         if (!req.user.admin) {
             errors.notadmin = 'Only admin can create things there'
@@ -67,15 +80,29 @@ router.post(
                     errors.category = 'Category with that ID is not longer exists'
                     res.status(400).json(errors);
                 } else {
-                    Product.create({
-                        name: req.body.name,
-                        category: req.body.category,
-                        price: req.body.price,
-                        description: req.body.description,
-                        image: req.body.image
-                    })
-                    .then(product => res.json(product))
-                    .catch(err => console.log(err));
+                    if (productId) {
+                        // Update
+                        Product.findByIdAndUpdate(productId, {
+                            name: req.body.name,
+                            category: req.body.category,
+                            price: req.body.price,
+                            description: req.body.description,
+                            image: req.body.image
+                        })
+                        .then(product => res.json(product))
+                        .catch(err => console.log(err));
+                    } else {
+                        // Create
+                        Product.create({
+                            name: req.body.name,
+                            category: req.body.category,
+                            price: req.body.price,
+                            description: req.body.description,
+                            image: req.body.image
+                        })
+                        .then(product => res.json(product))
+                        .catch(err => console.log(err));
+                    }
                 }
             })
             .catch(err => console.log(err));
